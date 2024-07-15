62e77711c0c2ac20b30de163f179c5c6e19cc2ea
Abhinavsharma2001 <123074669+Abhinavsharma2001@users.noreply.github.com>
2024-07-15 23:25:04 +0530

    Add files via upload

---
+++ E commerce/script.js
@@ -0,0 +1,9 @@
+/*document.querySelector("#show-contact us").addEventListener("click",function(){
+    document.querySelector(".container_1").classList.add("active");
+
+});
+document.querySelector(".container_1 .close-btn").addEventListener("click",function(){
+    document.querySelector(".container_1").classList.remove("active");
+
+}); */
+
